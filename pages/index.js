a417a8ffe2c32803a9c9e91828728430994b0fff
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-12 19:38:11 +0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Mobile Diagnoser</h1>
+      <p>Connect your Android phone via USB and start diagnosis.</p>
+    </div>
+  );
+}
